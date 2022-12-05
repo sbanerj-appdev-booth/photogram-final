ddf20f1583543dada192c94e0e323e8cf83bdfa6
banerso <sbanerje@chicagobooth.edu>
2022-12-05 02:12:45 +0000

    Photogram_final_users done

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_12_05_021042) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "username"
+    t.string "password_digest"
+    t.boolean "private"
+    t.integer "likes_count"
+    t.integer "comments_count"
+    t.integer "sent_follow_requests_count"
+    t.integer "received_follow_requests_count"
+    t.integer "own_photos_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
